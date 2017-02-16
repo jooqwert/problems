2903fb9a06cc72c2c91f39e55c0a988c2dbebe71
JooKyungMin <jooqwert@gmail.com>
2017-02-16 18:31:58 +0900

    11866 edit

--- queues/queue4/queue.cpp
+++ queues/queue4/queue.cpp
@@ -1,6 +1,51 @@
 /* 조세퍼스 문제0 : https://www.acmicpc.net/problem/11866 */
+/* #원형큐 */
+#include <stdio.h>
+#include <list>
+#include <queue>
+using namespace std;
+
+int main()
+{
+	int N,M;
+	scanf("%d%d", &N,&M);
+	queue<int> q;
+	for (int i = 0; i < N; ++i)
+	{
+		q.push(i+1);
+	}
+
+	printf("<");
+	while(!q.empty())
+	{
+		for (int j = 1; j < M; ++j)
+		{
+			q.push(q.front());
+			q.pop();  
+		}
+		printf("%d", q.front());
+		q.pop();
+		if (0 < q.size())
+		{
+			printf(", ");
+		}
+	}
+	printf(">");
+
+	return 0;
+}
+
+
+
+
+
+
+
+
 /* 조세퍼스 문제 0 : https://www.acmicpc.net/problem/11866 */
 /* #반복자#iterator#원형#순환 */
+
+/*
 #include <stdio.h>
 #include <list>
 #include <iostream>
@@ -47,3 +92,4 @@ int main()
 
 	return 0;
 }
+*/

046b63a25bc23fccea14bb38bb7d6863b5912f0e
JooKyungMin <jooqwert@gmail.com>
2017-03-01 16:14:15 +0900

    problem 6591 solve

--- combination/7/combination.cpp
+++ combination/7/combination.cpp
@@ -1,55 +1,51 @@
 /* 이항 쇼다운 : https://www.acmicpc.net/problem/6591 */
-/* #재귀#이항계수*/
+/* #이항계수 #메모리 #최소화 #팩토리얼 */
 #include <cstdio>
 #include <cstdlib>
 
 using namespace std;
 
-
-int ** memo;
-
-int combination(int n, int k)
-{
-	if (memo[n][k] == 0)
-	{
-
-		memo[n][k] = combination(n - 1, k - 1) + combination(n - 1, k);
-	}	
-
-	return memo[n][k]; 
-}
-
-
 int main()
 {
-	int N,K;
+	long long N,K, result;
 	while(true)
 	{
-		scanf("%d%d", &N, &K);
+		scanf("%lld%lld", &N, &K);
 		if (N==0&&K==0)
 		{
 			break;
 		}
-		if(K> N/2)
+		
+		if(K > N/2)
 		{
 			K=N-K;
 		}
-		memo = (int **) calloc(N + 1, sizeof(int *));
-		for (int i = 0; i < N+1; ++i)
+	
+		result = 1;
+		long long * k = (long long *) calloc (K, sizeof(long long));
+		for (long long i = 0; i < K; ++i)
 		{
-			memo[i] = (int *) calloc(K+1, sizeof(int));
-			memo[i][0] = 1;
-			if (i<=K)
+			k[i] = i+1;
+		} 
+
+		for (long long i = N; i > N-K; --i)
+		{
+
+			result = result * i;
+
+			for (long long j = 0; j < K; ++j)
 			{
-				memo[i][i] = 1;
-			}
+				if (k[j] !=0 && result % k[j] == 0)
+				{
+					result = result / k[j];
+					k[j] = 0;
+				}
+			} 
+
+
 		}
-		printf("%d\n", combination(N, K));
-		for (int i = 0; i < N+1; ++i)
-		{
-			free(memo[i]);			
-		}
-		free(memo);
+
+		printf("%lld\n", result);
 	}
 	return 0;
 }
